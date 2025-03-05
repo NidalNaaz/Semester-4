d31b5fd4d317627e7e4ec4982f20298356fc1e5c
NidalNaaz <147641097+NidalNaaz@users.noreply.github.com>
2025-03-05 15:30:19 +0530

    Create Dictionary.py

---
+++ Numerical Methods/Lab Cycle/Dictionary.py
@@ -0,0 +1,8 @@
+list1 = { "Name" : "Nidaal" , "Age" : 20 , "College" : "CUSAT" } 
+list2 = { "Name" : "Nidal" , "Age" : 20 , "College" : "CUSAT" } 
+list3 = { "Name" : "Nidukka" , "Age" : 20 , "College" : "CUSAT" } 
+
+listofDictionary = [ list1 , list2 , list3 ] 
+
+for i in listofDictionary :
+    print ( i )
